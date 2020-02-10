8bc6ad6ef0579798698b6965cc2521ef23a079b7
Sahil <spapalk@ncsu.edu>
2020-02-10 18:31:15 -0500

    Items scaffold added

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_09_012654) do
+ActiveRecord::Schema.define(version: 2020_02_10_225613) do
 
   create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "items", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
