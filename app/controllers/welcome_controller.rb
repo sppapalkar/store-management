55595af5ccad26f09d577fcfd8c2a3ecd3049d9d
Sahil <spapalk@ncsu.edu>
2020-02-27 15:41:25 -0500

    Home page updated

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,10 @@ class WelcomeController < ApplicationController
   def index
     if not current_user.nil? and current_user.admin
       redirect_to admin_path
+    else
+      items = Item.all
+      @new_items = items.order(:created_at).limit(5)
+      @popular_items = items.order(:popularity).limit(5)
     end
   end
 end
