fd8246816b2c7bb662157337160faec8e1c755ea
Sahil <spapalk@ncsu.edu>
2020-02-27 15:55:32 -0500

    Minor changes

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,8 +4,8 @@ class WelcomeController < ApplicationController
       redirect_to admin_path
     else
       items = Item.all
-      @new_items = items.order(:created_at).limit(5)
-      @popular_items = items.order(:popularity).limit(5)
+      @new_items = items.order('created_at DESC').limit(4)
+      @popular_items = items.order('popularity DESC').limit(4)
     end
   end
 end
